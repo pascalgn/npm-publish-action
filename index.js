7f2bcd7a3beee474be76934a7e2318a20e2bdbac
Theo <theo.cousinet@epitech.eu>
2021-03-20 22:38:40 +0100

    Add context output after run

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const process = require("process");
+const os = require("os");
 const { join } = require("path");
 const { spawn } = require("child_process");
 const { readFile } = require("fs");
@@ -17,7 +18,7 @@ async function main() {
     getEnv("COMMIT_PATTERN") || "^(?:Release|Version) (\\S+)";
 
   const createTagFlag = getEnv("CREATE_TAG") !== "false";
-  
+
   const publishCommand = getEnv("PUBLISH_COMMAND") || "yarn";
   const publishArgs = arrayEnv("PUBLISH_ARGS");
 
@@ -70,14 +71,17 @@ async function processDirectory(dir, config, commits) {
 
   const { version } = packageObj;
 
-  checkCommit(config, commits, version);
+  const foundCommit = checkCommit(config, commits, version);
 
   if (config.createTag) {
-    await createTag(dir, config, version);  
+    await createTag(dir, config, version);
   }
-  
+
   await publishPackage(dir, config, version);
 
+  setOutput('changed', true)
+  setOutput('version', version)
+  setOutput('commit', foundCommit.id)
   console.log("Done.");
 }
 
@@ -86,7 +90,7 @@ function checkCommit(config, commits, version) {
     const match = commit.message.match(config.commitPattern);
     if (match && match[1] === version) {
       console.log(`Found commit: ${commit.message}`);
-      return;
+      return commit;
     }
   }
   throw new NeutralExitError(`No commit found for version: ${version}`);
@@ -150,6 +154,27 @@ async function publishPackage(dir, config, version) {
   console.log("Version has been published successfully:", version);
 }
 
+function setOutput(name, value = '') {
+  if (typeof name !== "string") name = JSON.stringify(name);
+  if (typeof value !== "string") value = JSON.stringify(value);
+  
+  const cmd = `::set-output name=${escapeProperty(name)}::${escapeData(value)}`;
+  process.stdout.write(cmd + os.EOL);
+}
+
+function escapeProperty(s) {
+  return s
+    .replace(/%/g, "%25")
+    .replace(/\r/g, "%0D")
+    .replace(/\n/g, "%0A")
+    .replace(/:/g, "%3A")
+    .replace(/,/g, "%2C");
+}
+
+function escapeData(s) {
+  return s.replace(/%/g, "%25").replace(/\r/g, "%0D").replace(/\n/g, "%0A");
+}
+
 function run(cwd, command, ...args) {
   console.log("Executing:", command, args.join(" "));
   return new Promise((resolve, reject) => {
@@ -188,6 +213,7 @@ class NeutralExitError extends Error {}
 
 if (require.main === module) {
   main().catch(e => {
+    setOutput('changed', false)
     if (e instanceof NeutralExitError) {
       // GitHub removed support for neutral exit code:
       // https://twitter.com/ethomson/status/1163899559279497217
